0b7d13fec7ee309b579ec32359963ca0982b0382
rebecca-salg <rebeccasalg@hotmail.com>
2021-06-07 14:12:22 +0200

    decrementsNumber

--- src/git/Main.java
+++ src/git/Main.java
@@ -4,10 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(plus(1));
+        System.out.println(minus(1));
     }
 
     public static int plus(int a) {
         return a ++;
 
+    }
+    public static int minus(int b) {
+        return b--;
     }
 }
