4ccbdcb4854ec296b1a29e9ba590d25f5a291325
rebecca-salg <rebeccasalg@hotmail.com>
2021-06-07 14:08:25 +0200

    incrementsNumber

---
+++ src/git/Main.java
@@ -0,0 +1,13 @@
+package git;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(plus(1));
+    }
+
+    public static int plus(int a) {
+        return a ++;
+
+    }
+}
